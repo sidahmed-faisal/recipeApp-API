9431d30f84d41b14fc5801a51f75c7b01e83252b
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-08-07 11:48:05 +0200

    added the wait_for_db to doecker-compose

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -24,7 +24,7 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         # check if check method is called
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep ,patched_check):
@@ -37,5 +37,5 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
         
