1b1ff1f37ffe563c42447fe7e8cc5b7519cd61e5
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-10-03 20:24:55 +0200

    added the customization of the basic django admin fields

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django Admin Customization
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -10,5 +11,20 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users"""
     ordering = ['id']
     list_display = ['email','name']
+    fieldsets = (
+        (None , {'fields': ('email','password')}) ,
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'),{'fields':('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 admin.site.register(models.User, UserAdmin)
